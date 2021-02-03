fea933a7fc91ae12d538a64384ba44131c50463c
Ivan Gonzalez <ivan@iconos.mx>
2021-02-02 18:50:05 -0800

    Columnas y estilos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  
+  final TextStyle estiloTexto = TextStyle(fontSize: 25.0);
 
   // build snipets
-  
   @override
   Widget build(BuildContext context) {
-
     // ver doc en flutter
     return Scaffold(
       appBar: AppBar(
@@ -14,8 +14,15 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Body')
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Text('Numero de clicks', style: TextStyle(fontSize: 25.0)),
+          // Text('0',style: TextStyle(fontSize: 25.0)),
+          Text('Numero de clicks', style: estiloTexto),
+          Text('0',style: estiloTexto),
+        ],
+      )),
     );
   }
 }
