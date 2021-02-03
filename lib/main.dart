6ad58378a0d2175584803489000caaf715ab158e
Ivan Gonzalez <ivan@iconos.mx>
2021-02-02 18:31:30 -0800

    Separando en archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
 
+// import 'package:contador/src/app.dart';
+import 'src/app.dart';
+
 void main() {
   // correr la aplicacion
   runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(context) {
-    // Ver docs de flutter
-    // Text primero es posicional
-    // HOT RESTAR
-
-    // return MaterialApp(home: Text('Algun Widget'));
-
-  //
-    return MaterialApp(
-      home: Center(
-        child: Text('Hola mundo')
-      )
-    );
-  }
 }
