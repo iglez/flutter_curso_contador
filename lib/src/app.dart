f736ade19aa5516888560b9bbf435e10fe461ed9
Ivan Gonzalez <ivan@iconos.mx>
2021-02-02 18:42:40 -0800

    no message

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:contador/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,12 +9,11 @@ class MyApp extends StatelessWidget {
     // HOT RESTAR
 
     // return MaterialApp(home: Text('Algun Widget'));
-
-  //
     return MaterialApp(
-      home: Center(
-        child: Text('Hola mundo!')
-      )
+      // home: Center(
+      //   child: Text('Hola mundo!!')
+      // )
+      home: HomePage()
     );
   }
 }
